70c32894232b2c4550b3991610542adad800d517
timf34 <farrelti@tcd.ie>
2022-04-13 23:13:06 +0100

    Blockchain refactoring done

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,17 @@
 from block import Block
-import hashlib
-
+import time
 
 class Blockchain:
     def __init__(self, difficulty: int = 2):
         self.chain = []
-        self.current_transactions = []
+        self.unconfirmed_transactions = []
         self.difficulty = difficulty
 
     def create_genesis_block(self):
         """
         Creates the genesis block
         """
-        genesis_block = Block(0, [], "0")
+        genesis_block = Block(0, [], 0, "0")
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
 
@@ -36,18 +35,7 @@ class Blockchain:
         self.chain.append(block)
         return True
 
-    def is_valid_proof(self, block: Block, proof: str) -> bool:
-        """
-        Validates the proof of work
-        """
-        """guess = f'{block.previous_hash}{str(proof)}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:self.difficulty] == "0" * self.difficulty"""
-        # Note: this was the other problem, see the above. And the solution below. Proof is the block's hash which
-        #  we pass through
-        return proof.startswith('0' * self.difficulty) and proof == block.compute_hash()
-
-    def proof_of_work(self, block: Block) -> int:
+    def proof_of_work(self, block: Block) -> str:
         """
         Finds a valid proof of work for the block
         """
@@ -71,13 +59,20 @@ class Blockchain:
         """
         Adds a new transaction to the list of transactions
         """
-        self.current_transactions.append(transaction) # equal to unconfirmed_transactions
+        self.unconfirmed_transactions.append(transaction)
+
+    def is_valid_proof(self, block, block_hash) -> bool:
+        """
+        Check if block_hash is valid hash of block and satisfies
+        the difficulty criteria.
+        """
+        return (block_hash.startswith('0' * self.difficulty) and
+                block_hash == block.compute_hash())
 
     def is_valid_chain(cls, chain: list) -> bool:
         """
         Validates the chain
         """
-        # TODO: test this method
         last_block = chain[0]
         current_index = 1
         while current_index < len(chain):
@@ -93,18 +88,21 @@ class Blockchain:
 
     def mine(self) -> bool:
         """
-        Mines a new block
+        This function serves as an interface to add the pending
+        transactions to the blockchain by adding them to the block
+        and figuring out Proof Of Work.
         """
-        if not self.current_transactions:
+        if not self.unconfirmed_transactions:
             return False
+
         last_block = self.last_block
-        print("last_block.hash: ", last_block.hash)
         new_block = Block(index=last_block.index + 1,
-                          transactions=self.current_transactions,
+                          transactions=self.unconfirmed_transactions,
+                          timestamp=time.time(),
                           previous_hash=last_block.hash)
         proof = self.proof_of_work(new_block)
         self.add_block(new_block, proof)
-        self.current_transactions = []
+        self.unconfirmed_transactions = []
         return True
 
     def __repr__(self):
